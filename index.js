437f6e500f9be5d4df4c610f7249387b2ea6ccf8
Yousif <yousufalkhateeb@gmail.com>
2022-03-27 01:41:27 -0600

    Added query page, list page, and upload page. Need to format date correctly.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // server
 const express = require('express');
-const http = require ('http');
 const mongoose = require('mongoose');
 let app = express();
 const port = 3000;
@@ -8,37 +7,59 @@ const port = 3000;
 // database
 mongoose.connect('mongodb://localhost:27017/test');
 let userModel = require("./models/users");
-console.log(userModel.listAllUsers());
-
+let queryModel = require("./models/query");
 
 // setting server
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
 app.use(express.json());
  
-app.get("/form", function(req, res){
-    res.render("form");
-})
-app.get("/users", function(req, res){
+app.get("/upload", function(req, res){
+    res.render("upload");
+});
+app.get("/list", function(req, res){
     userModel.listAllUsers().then(function(users){
-        res.render("users", {users: users})
+        res.render("list", {users: users});
     }).catch(function(error){
         res.error("Something went wrong! " + error);
     });
-})
-app.post("/user", function(req, res){
+    
+});
+app.post("/allUsers", function(req, res){
     console.log("User: " + JSON.stringify(req.body.user));
-    let newUser = new userModel(req.body.car);
+    let newUser = new userModel(req.body.user);
 
     newUser.save().then(function(){
-        res.send("Added new user to database!");
+        console.log("Added new user to database!");
     }).catch(function(error){
-        res.err("Failed to add new user to database!");
+        console.log("Failed to add new user to database!");
     });
+    res.render("upload");
+});
+app.get("/query", function(req, res){
+    res.render("query");
+});
+
+app.post("/queryResult", function(req, res){
+    let filter = req.body.query;
+    if(filter.selection == "age"){
+        if(filter.condition == 'gt'){
+            queryModel.listAgeGT(filter.number).then(function(users){
+                res.render("queryResult", {users: users});
+            })
+        }
+        else if(filter.condition == 'lt'){
+            queryModel.listAgeLT(filter.number).then(function(users){
+                res.render("queryResult", {users: users});
+            })
+        }
+        else if(filter.condition == 'et'){
+            queryModel.listAgeET(filter.number).then(function(users){
+                res.render("queryResult", {users: users});
+            })
+        }
+    }
 })
-
-
-
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- models/users.js
+++ models/users.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 let userSchema = new mongoose.Schema({
     name: String,
     age: Number,
-    major: String
+    major: String,
+    date: Date
 });
 
 userSchema.statics.listAllUsers = function() {
     return this.find({});
 };
-
-let userModel = mongoose.model('user', userSchema);
+let userModel = mongoose.model('users', userSchema);
 
 module.exports = userModel;
